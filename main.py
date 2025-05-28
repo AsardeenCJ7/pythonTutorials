068cbd3e4fed795857c5347fa2e7d3450f812d79
M.A. Asardeen <asardeenbse@gmail.com>
2025-05-28 12:35:52 +0530

    basics tutorial

---
+++ main.py
@@ -0,0 +1,139 @@
+# # # print("Hello World!") Here Single Line Commands
+# # """
+# # This is Multi Line Commands
+# # """
+# #
+# # # print("Hello World" , "Python Awesome! ", sep= "", end= "\n")
+# # # print("Customize this templates")
+# #
+# #
+# # # print(2+4)
+# # # print(1-3)
+# # # print(1*3)
+# # # print(1)
+# #
+# #
+# # # "Hi Welcome Python" or 'Hello Welcome'
+# # # "4" or '4'  or 4
+# # # Data Types
+# #
+# # # print(4+5)  # 9
+# # # print("4+5") #4+5
+# # # print("Hello World")
+# # print("Hello")  # print(Hello);  print(500!)  print(100A)
+# #
+# # print(100)
+# # print("100")    #100+50 Tamil adfiafiafai
+# # print("100+50")
+# #
+# # print("I'm Asardeen")  #I'm Asardeen
+# # print('I"m Asardeen') #I"m Asardeen
+# #
+# #
+# #
+# # # Data 1.String = "" or ''
+# # # Integer 0 - 1233
+# # # "100"  == 100
+# # print("100")
+# # print ("200+50")
+# # print(200+50)
+#
+# # print("100" + 50) # + between Numbers + add
+# print("100" + "50") # 10050
+# print(100+50) # 150  Integer + Addition
+# print("Hi"+"100"+"Abc"+30) # Hi100Abc  String + Concatenation
+#
+
+# print("Welcome to Learn Python")  #instruction --> Console - Output
+# print(1+3)
+# print("1+3") # " String " or ' String '
+# print(type("Hello"))   #String
+# print(type('a')) #String
+# print(type(1)) # Int
+# print(type(-1)) #Int
+# print(type(-4.512544865245255)) # Float
+
+# num = "-4.1f" # string "" or '' abc123@
+# print(type(num))
+#String and Integer
+#Sequence of characters called String   ""  or ''
+#Integer - Whole Number 12345648 -1 -2 -3 0
+# 1.5, -2.568, -7.8
+
+# print("405/C, Hijra Vidyalaya Road")
+# print(450+550)
+
+# print("*")
+# print("**")
+# print("***")
+# print("****")
+#
+# studentName1 = "Anshak Ahamed"
+# address1 = "Main Road, Oddamvadi"
+#
+# studentName2 = "Afeef"
+# address2 = "Mavadicheanai"
+#
+# studentName3 = "Hamthi"
+#
+#
+# print(studentName1)
+# print(studentName2)
+# print(studentName3)
+
+
+# num = input("Enter Your Number : ")  // 15
+# print(num + 2)   #print( "15" + 2)
+
+# print(15 + 2) #addition
+# print ("15" + "2")  #152 # Concatenation
+
+#
+# num1 = int(input("Enter Number 1 : ") )
+# num2 = int(input("Enter Number 2 : ")  )## 1
+# print(num1 + num2) # 551
+#
+# # print("550" + "1")
+# # 5501
+
+
+# print("************************************************")
+# print("Welcome to Amana Bank")
+# bankPin = 2565
+# pinNumber = int(input("Enter Your 4 Digit PIN Number : "))
+# if(bankPin == pinNumber):
+#     print("1. Check Your Account Balance")
+#     print("2. Withdraw Balance")
+# else:
+#     print("Invalid Pin Number")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
